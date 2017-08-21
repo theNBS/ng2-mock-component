b4e7fc4973c941050a6fb024f1a62c1361892694
Alan Smith <alan.smith@ribaenterprises.com>
2017-08-21 13:43:36 +0100

    Fixed tslin errors

--- index.ts
+++ index.ts
@@ -8,19 +8,19 @@ import { Component, EventEmitter } from '@angular/core';
  * See https://angular.io/docs/ts/latest/api/core/index/Component-decorator.html for a list
  * of supported properties.
  */
-
+/* tslint:disable-next-line */
 export function MockComponent(options: Component): Component {
 
   const metadata: Component = {
     selector: options.selector,
     template: options.template || '',
     inputs: options.inputs,
-    outputs: options.outputs || []
+    outputs: options.outputs || [],
   };
 
   class Mock {}
 
-  metadata.outputs.forEach(method => {
+  metadata.outputs.forEach((method) => {
     Mock.prototype[method] = new EventEmitter<any>();
   });
 
